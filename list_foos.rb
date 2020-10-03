6ec8abc2a5371b21fc130a24d67381b7a7a0d6e1
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2020-10-03 00:28:57 -0500

    Add list_foos script

---
+++ list_foos.rb
@@ -0,0 +1,8 @@
+require 'gruf'
+
+require_relative 'test_services_pb.rb'
+
+client = Gruf::Client.new(service: Test::Test, options: { hostname: '0.0.0.0:9001' })
+client.call(:ListFoos, {}).tap do |resp|
+  resp.message.each { |foo| puts foo.name }
+end
