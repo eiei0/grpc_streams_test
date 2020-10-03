e25079ae503f67d375831f366ef1f7287afa56d1
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2020-10-02 23:56:48 -0500

    Client stream endpoint

--- app/rpc/test_controller.rb
+++ app/rpc/test_controller.rb
@@ -7,6 +7,12 @@ require_relative "../models/foo_enum"
 class TestController < ::Gruf::Controllers::Base
   bind Test::Test::Service
 
+  def create_foos
+    request.message.call { |req| puts req.name }
+
+    Test::EmptyResp.new
+  end
+
   def list_foos
     foos = 100.times.map { |i| Foo.new(name: "foo_#{i}") }
     FooEnum.new(foos).each
