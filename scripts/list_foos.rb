41fb9684cfcef8cb0f6c21df4179e5fda1a73e1e
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2020-10-07 13:46:00 -0500

    Print foos in baches of 1000 with sleep

--- scripts/list_foos.rb
+++ scripts/list_foos.rb
@@ -4,5 +4,5 @@ require_relative '../test_services_pb.rb'
 
 client = Gruf::Client.new(service: Test::Test, options: { hostname: '0.0.0.0:9001' })
 client.call(:ListFoos, {}).tap do |resp|
-  resp.message.each { |foo| puts foo.name }
+  resp.message.each_slice(1000) { |foos| foos.each { |foo| puts foo.name }; sleep 2 }
 end
